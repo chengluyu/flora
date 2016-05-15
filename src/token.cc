0fb3500bfedfabc8e6d2b0965a538bd2f946e3a6
Cheng Luyu <chengluyu@live.cn>
2016-05-15 23:47:24 +0800

    Add a function to test if an identifier is keyword.

--- src/token.cc
+++ src/token.cc
@@ -2,22 +2,39 @@
 
 namespace flora {
 
+Token Tokens::LookupKeyword(const std::string &identifier) {
+  auto result = lookup_table_.find(identifier);
+  return result == lookup_table_.end() ? result->second : Token::Identifier;
+}
+
 #define T(name, literal, precedence) #name
 const char* const Tokens::name_[Tokens::TOKEN_COUNT] = {
-  TOKEN_LIST(T)
+  TOKEN_LIST(T, T)
 };
 #undef T
 
 #define T(name, literal, precedence) #literal
 const char* const Tokens::literal_[Tokens::TOKEN_COUNT] = {
-  TOKEN_LIST(T)
+  TOKEN_LIST(T, T)
 };
 #undef T
 
 #define T(name, literal, precedence) precedence
 const int Tokens::precedence_[Tokens::TOKEN_COUNT] = {
-  TOKEN_LIST(T)
+  TOKEN_LIST(T, T)
 };
 #undef T
+
+void Tokens::InitializeKeywordLookupTable() {
+#define K(name, literal, precedence) lookup_table_[literal] = Token::name;
+#define T(name, literal, precedence)
+  TOKEN_LIST(K, T)
+#undef K
+#undef T
+}
+
+Tokens::Tokens() {
+  InitializeKeywordLookupTable();
+}
 
 }
--- src/token.h
+++ src/token.h
@@ -1,63 +1,66 @@
 #ifndef FLORA_TOKEN_H
 #define FLORA_TOKEN_H
 
+#include <string>
+#include <unordered_map>
+
 #include "flora.h"
 
 namespace flora {
 
-#define TOKEN_LIST(K)\
+#define TOKEN_LIST(K, T)\
   K(EndOfSource)\
-  /* Punctuation marks */ \
-  K(LeftParenthesis, "(", 0)\
-  K(RightParenthesis, ")", 0)\
-  K(LeftBracket, "[", 0)\
-  K(RightBracket, "]", 0)\
-  K(LeftBrace, "{", 0)\
-  K(RightBrace, "}", 0)\
-  K(Colon, ":", 0)\
-  K(Semicolon, ";", 0)\
-  K(Period, ".", 0)\
-  K(Ellipsis, "...", 0)\
-  K(Conditional, "?", 0)\
-  K(Increment, "++", 0)\
-  K(Decrement, "--", 0)\
-  K(Arrow, "=>", 0)\
+  /* Punctuation marks */\
+  T(LeftParenthesis, "(", 0)\
+  T(RightParenthesis, ")", 0)\
+  T(LeftBracket, "[", 0)\
+  T(RightBracket, "]", 0)\
+  T(LeftBrace, "{", 0)\
+  T(RightBrace, "}", 0)\
+  T(Colon, ":", 0)\
+  T(Semicolon, ";", 0)\
+  T(Period, ".", 0)\
+  T(Ellipsis, "...", 0)\
+  T(Conditional, "?", 0)\
+  T(Increment, "++", 0)\
+  T(Decrement, "--", 0)\
+  T(Arrow, "=>", 0)\
   /* Assignment operators */\
-  K(Assignment, "=", 2)\
-  K(AssignmentBitwiseOr, "|=", 2)\
-  K(AssignmentBitwiseXor, "^=", 2)\
-  K(AssignmentBitwiseAnd, "&=", 2)\
-  K(AssignmentShiftLeft, "<=<",2)\
-  K(AssignmentShiftRight, ">=>",2)\
-  K(AssignmentAddition, "+=", 2)\
-  K(AssignmentSubtraction, "-=", 2)\
-  K(AssignmentMultiplication, "*=", 2)\
-  K(AssignmentDivision, "/=", 2)\
-  K(AssignmentModulus, "%=", 2)\
+  T(Assignment, "=", 2)\
+  T(AssignmentBitwiseOr, "|=", 2)\
+  T(AssignmentBitwiseXor, "^=", 2)\
+  T(AssignmentBitwiseAnd, "&=", 2)\
+  T(AssignmentShiftLeft, "<<=",2)\
+  T(AssignmentShiftRight, ">>=",2)\
+  T(AssignmentAddition, "+=", 2)\
+  T(AssignmentSubtraction, "-=", 2)\
+  T(AssignmentMultiplication, "*=", 2)\
+  T(AssignmentDivision, "/=", 2)\
+  T(AssignmentModulus, "%=", 2)\
   /* Binary operators */\
-  K(Comma, ",", 1)\
-  K(LogicalOr, "||", 4)\
-  K(LogicalAnd, "&&", 4)\
-  K(BitwiseOr, "|", 6)\
-  K(BitwiseXor, "^", 7)\
-  K(BitwiseAnd, "&", 8)\
-  K(ShiftLeft, "<<", 11)\
-  K(ShiftRight, ">>", 11)\
-  K(Addition, "+", 12)\
-  K(Subtraction, "-", 12)\
-  K(Multiplication, "*", 12)\
-  K(Division, "/", 13)\
-  K(Modulus, "%", 13)\
+  T(Comma, ",", 1)\
+  T(LogicalOr, "||", 4)\
+  T(LogicalAnd, "&&", 4)\
+  T(BitwiseOr, "|", 6)\
+  T(BitwiseXor, "^", 7)\
+  T(BitwiseAnd, "&", 8)\
+  T(ShiftLeft, "<<", 11)\
+  T(ShiftRight, ">>", 11)\
+  T(Addition, "+", 12)\
+  T(Subtraction, "-", 12)\
+  T(Multiplication, "*", 12)\
+  T(Division, "/", 13)\
+  T(Modulus, "%", 13)\
   /* Compare operators */\
-  K(Equal, "==", 9)\
-  K(NotEqual, "!=", 9)\
-  K(LessThan, "<", 10)\
-  K(GreaterThan, ">", 10)\
-  K(LessThanOrEqual, "<=", 10)\
-  K(GreaterThanOrEqual, ">=", 10)\
+  T(Equal, "==", 9)\
+  T(NotEqual, "!=", 9)\
+  T(LessThan, "<", 10)\
+  T(GreaterThan, ">", 10)\
+  T(LessThanOrEqual, "<=", 10)\
+  T(GreaterThanOrEqual, ">=", 10)\
   /* Unary operators */\
-  K(Not, "!", 0)\
-  K(BitwiseNot, "~", 0)\
+  T(LogicalNot, "!", 0)\
+  T(BitwiseNot, "~", 0)\
   /* Keywords */\
   K(As, "as", 0)\
   K(Break, "break", 0)\
@@ -95,14 +98,14 @@ namespace flora {
   K(Null, "null", 0)\
   K(True, "true", 0)\
   K(False, "false", 0)\
-  K(Integer, nullptr, 0)\
-  K(RealNumber, nullptr, 0)\
-  K(Character, nullptr, 0)\
-  K(String, nullptr, 0)\
+  T(Integer, nullptr, 0)\
+  T(RealNumber, nullptr, 0)\
+  T(Character, nullptr, 0)\
+  T(String, nullptr, 0)\
   /* Identifier */\
-  K(Identifier, nullptr, 0)\
+  T(Identifier, nullptr, 0)\
   /* Illegal token */\
-  K(Illegal, nullptr, 0)
+  T(Illegal, nullptr, 0)
 
 
 enum class Token : int {
@@ -113,8 +116,7 @@ enum class Token : int {
 };
 
 class Tokens {
-
-  STATIC_CLASS(Tokens)
+public:
 
   static const char* Name(Token token) {
     return name_[static_cast<int>(token)];
@@ -128,12 +130,21 @@ class Tokens {
     return precedence_[static_cast<int>(token)];
   }
 
-private:
-  static const int TOKEN_COUNT = static_cast<int>(TOKEN_COUNT);
+  static Token LookupKeyword(const std::string &identifier);
 
+private:
+  // The number of tokens
+  static const int TOKEN_COUNT = static_cast<int>(TOKEN_COUNT);
+  // Token information
   static const char *name_[TOKEN_COUNT];
   static const char *literal_[TOKEN_COUNT];
   static const int precedence_[TOKEN_COUNT];
+  // Hashtable used to lookup keywords
+  static const std::unordered_map<std::string, Token> lookup_table_;
+  static void InitializeKeywordLookupTable();
+  // Singleton object used to automatically initialize hashtable
+  Tokens();
+  static Tokens singleton;
 };
 
 }

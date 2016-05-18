48e062b11ed7c2647e7b3b24aac24111eb7df599
Cheng Luyu <chengluyu@live.cn>
2016-05-18 19:37:05 +0800

    Add checks.h, which contains some basic error check functions.

---
+++ src/checks.h
@@ -0,0 +1,15 @@
+#ifndef FLORA_CHECKS_H
+#define FLORA_CHECKS_H
+
+#include <cstdlib>
+#include <cstdio>
+
+void flora_fatal(const char *file, int line, const char *message) {
+  std::printf("FATAL ERROR at line %d in file '%s': %s", line, file, message);
+  std::abort();
+}
+
+#define UNREACHABLE() flora_fatal(__FILE__, __LINE__, "unreachable code")
+#define UNIMPLEMENTED() flora_fatal(__FILE__, __LINE__, "unimplemented code")
+
+#endif
